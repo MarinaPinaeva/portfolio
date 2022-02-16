fc9b1ba1e775a4383f17f68a30063b2df2f3137a
Marina <example@gmail.com>
2022-02-16 19:03:55 +0300

    second window

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,21 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 400) {
+        $('.sidepanel').addClass('active');
+        $('.sidepanel__divider').addClass('active');
+        $('#facebook').addClass('active');
+        $('#instagram').addClass('active');
+        $('#github').addClass('active');
+    }
+
+    if ($(this).scrollTop() < 400) {
+        $('.sidepanel').removeClass('active');
+        $('.sidepanel__divider').removeClass('active');
+        $('#facebook').removeClass('active');
+        $('#instagram').removeClass('active');
+        $('#github').removeClass('active');
+    }
+    
+});
