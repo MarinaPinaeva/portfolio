973d1463cf6e597f09768e4d42c2e2f8bd253a04
Marina <example@gmail.com>
2022-02-17 20:53:00 +0300

    fourth window

--- docs/js/script.js
+++ docs/js/script.js
@@ -28,3 +28,10 @@ $(window).scroll(function(){
     }
     
 });
+
+const counters = document.querySelectorAll('.skills__progress-counter'),
+        lines = document.querySelectorAll('.skills__progress-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
